9b74a310db0b34207e6a62d4be87cf3de40b0b27
Jacob Kaplan <jtkaplan@wpi.edu>
2018-08-30 15:42:50 -0400

    Primary Commit

---
+++ server.js
@@ -0,0 +1,36 @@
+var http = require('http')
+  , fs   = require('fs')
+  , url  = require('url')
+  , port = 8080;
+
+var server = http.createServer (function (req, res) {
+  var uri = url.parse(req.url)
+
+  switch( uri.pathname ) {
+    case '/':
+      sendFile(res, 'index.html')
+      break
+    case '/index.html':
+      sendFile(res, 'index.html')
+      break
+    case '/fancify.css':// Did someone say fancify?
+      sendFile(res, 'fancify.css')// Serve our style sheet
+      break //don't drip into the next case
+    default:
+      res.end('404 not found')
+  }
+})
+
+server.listen(process.env.PORT || port);
+console.log('listening on 8080')
+
+// subroutines
+
+function sendFile(res, filename) {
+
+  fs.readFile(filename, function(error, content) {
+    res.writeHead(200, {'Content-type': 'text/html'})
+    res.end(content, 'utf-8')
+  })
+
+}
